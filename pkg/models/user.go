318f97277cc351f5c10c96134ab662c10b1f3746
ym <wazap_by@mail.ru>
2022-07-02 18:08:09 +0200

    Deleted gorm, fix dir error

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,10 +1,7 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 type User struct {
-	gorm.Model
-	Username string `gorm:"size:255;not null;unique" json:"username"`
-	Password string `gorm:"size:255;not null;" json:"password"`
-	Longname string `gorm:"size:255;not null;" json:"longname"`
+	Username string `validate:"min=3, max=40, regexp=^[a-zA-Z0-9]*$" json:"username"`
+	Password string `validate:"min=3, max=40" json:"password"`
+	Longname string `validate:"min=4, max=40" json:"longname"`
 }
