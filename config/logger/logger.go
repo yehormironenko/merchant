5750090647e406236b5b72cad052e4207d4461e7
Yahor Mironenka <wazap_by@mail.ru>
2023-01-15 23:14:35 +0100

    modificated loggger

--- config/logger/logger.go
+++ config/logger/logger.go
@@ -1,15 +1,32 @@
 package logger
 
 import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 func InitLogger() {
 
 	//TODO change using flags
-	//zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
+	}
+	output.FormatMessage = func(i interface{}) string {
+		return fmt.Sprintf("***%s***", i)
+	}
+	output.FormatFieldName = func(i interface{}) string {
+		return fmt.Sprintf("%s:", i)
+	}
+	output.FormatFieldValue = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("%s", i))
+	}
+
+	log := zerolog.New(output).With().Timestamp().Logger()
 
 	//Test messages
 	log.Error().Msg("Error message")
