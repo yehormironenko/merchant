cb2a9b4f8825bb528e6cf7e68364a92b3e8bf43b
Yahor Mironenka <yahor.mironenka@bango.com>
2023-07-30 14:04:09 +0200

    updated gin version + added error if record exists in db

--- internal/repository/users_repo.go
+++ internal/repository/users_repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"fmt"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
@@ -10,6 +11,13 @@ import (
 
 	"merchant/config"
 	"merchant/internal/controllers/requests"
+)
+
+const (
+	username    = "username"
+	fullname    = "fullname"
+	email       = "email"
+	phoneNumber = "phoneNumber"
 )
 
 type userRepo struct {
@@ -29,16 +37,19 @@ func New(dynamoClient *dynamodb.Client, cfg config.Dynamo, logger *zerolog.Logge
 // RegisterUser create new user in database if user doesn't exist
 func (ur *userRepo) RegisterUser(ctx context.Context, request requests.RegisterUser) error {
 	ur.logger.Info().Object("with data", request).Msg("registering new user object")
+	condition := aws.String("attribute_not_exists(username) OR attribute_not_exists(email)") // TODO change the table
 
-	_, err := ur.db.PutItem(ctx, &dynamodb.PutItemInput{
+	resp, err := ur.db.PutItem(ctx, &dynamodb.PutItemInput{
 		TableName: aws.String(ur.tables.Users),
 		Item: map[string]types.AttributeValue{
-			"username":    &types.AttributeValueMemberS{Value: request.Username},
-			"fullname":    &types.AttributeValueMemberS{Value: fmt.Sprintf("%s %s", request.Firstname, request.Surname)},
-			"email":       &types.AttributeValueMemberS{Value: request.Email},
-			"phoneNumber": &types.AttributeValueMemberS{Value: request.PhoneNumber},
+			username:    &types.AttributeValueMemberS{Value: request.Username},
+			fullname:    &types.AttributeValueMemberS{Value: fmt.Sprintf("%s %s", request.Firstname, request.Surname)},
+			email:       &types.AttributeValueMemberS{Value: request.Email},
+			phoneNumber: &types.AttributeValueMemberS{Value: request.PhoneNumber},
 		},
+		ConditionExpression: condition,
 	})
+	fmt.Println(resp)
 	if err != nil {
 		ur.logger.Error().AnErr("error", err).Msg("register operation ended with an error")
 		return err
