d1bf1f2d6756890bf6af3022602bdc9a543d1b22
Yahor Mironenka <yahor.mironenka@bango.com>
2023-09-17 21:14:48 +0200

    - update populate file

--- init-scripts/populate.sh
+++ init-scripts/populate.sh
@@ -43,12 +43,16 @@ aws dynamodb --endpoint-url=http://10.11.0.3:8000 create-table \
 
 echo "################# Table created ###################"
 
+# Create 5 test items with UUIDv4 userId values
+aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item --table-name users --item '{"userId": {"S": "'0ebf0fdb-09cb-46c8-9eb9-12c83a540cd5'"}, "username": {"S": "user1"}, "fullname": {"S": "John Doe"}, "email": {"S": "user1@example.com"}, "phoneNumber": {"S": "+1234567890"}}'
 
-aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item  --table-name users  --item "{\"username\": {\"S\": \"user1\"}, \"fullname\": {\"S\": \"Mr. Bean\"}}"
+aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item --table-name users --item '{"userId": {"S": "'d8632d18-6286-4a7c-8ff7-5679a65234f6'"}, "username": {"S": "user2"}, "fullname": {"S": "Jane Smith"}, "email": {"S": "user2@example.com"}}'
 
-aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item  --table-name users  --item "{\"username\": {\"S\": \"user2\"}, \"fullname\": {\"S\": \"John Smith\"}}"
+aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item --table-name users --item '{"userId": {"S": "'8bca68dd-f85e-429d-9c99-5e9d8c05dc82'"}, "username": {"S": "user3"}, "fullname": {"S": "Alice Johnson"}, "email": {"S": "user3@example.com"}}'
 
-aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item  --table-name users  --item "{\"username\": {\"S\": \"user3\"}, \"fullname\": {\"S\": \"Jan Kowalski\"}}"
+aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item --table-name users --item '{"userId": {"S": "'753fc301-4d01-41e2-9c42-73f01eb4cbfe'"}, "username": {"S": "user4"}, "fullname": {"S": "Bob Brown"}, "email": {"S": "user4@example.com"}, "phoneNumber": {"S": "+9876543210"}}'
+
+aws --endpoint-url=http://10.11.0.3:8000 dynamodb put-item --table-name users --item '{"userId": {"S": "'ae129f55-a39b-4b55-b3aa-dbef3497da14'"}, "username": {"S": "user5"}, "fullname": {"S": "Eve Williams"}, "email": {"S": "user5@example.com"}}'
 
 echo "########### Selecting all data from a table ###########"
 aws dynamodb scan --endpoint-url=http://10.11.0.3:8000 --table-name users
